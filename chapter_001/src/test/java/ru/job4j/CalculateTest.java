1642eba873f56b908afe99b0860441d4a1a32a13
dvamedveda <justmustdie@yandex.ru>
2017-10-15 16:19:46 +0600

    добавил CalculateTest / 1.4. JUnit[#27973]

---
+++ chapter_001/src/test/java/ru/job4j/CalculateTest.java
@@ -0,0 +1,27 @@
+package ru.job4j;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+* Test
+* @author b.savelev
+* @version 2.0
+* @since 1.8
+*/
+public class CalculateTest {
+	
+	/**
+	* Test for echo method
+	*/
+	@Test
+	public void whenTakeNameThenThreeEchoPlusName() {
+		String input = "b.savelev";
+		String expect = "Echo, echo, echo: b.savelev";
+	
+		Calculate calc = new Calculate();
+		String result = calc.echo(input);
+		assertThat(result, is(expect));
+	}
+}
