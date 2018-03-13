0e03d576ad7d48714d1e8cab7612466ab875850f
dvamedveda <justmustdie@yandex.ru>
2018-03-14 01:16:31 +0600

    Завел новый модуль для выполнения заданий пакета Junior, а также пустые классы в нем для освоения команды git rebase. / 1. Объединение нескольких коммитов.[#44950]

---
+++ chapter_004/src/main/java/ru/job4j/rebase/Rebase.java
@@ -0,0 +1,12 @@
+package ru.job4j.rebase;
+
+/**
+ * Пустой класс, для обучения использованию git rebase.
+ *
+ * @author - b.savelev (mailto: justmustdie@yandex.ru)
+ * @version - 1.0
+ * @since 0.1
+ */
+public class Rebase {
+
+}
---
+++ chapter_004/src/main/java/ru/job4j/rebase/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * Пакет, содержащий тестовые файлы для обучения использованию git rebase.
+ *
+ * @author - b.savelev (mailto: justmustdie@yandex.ru)
+ * @version - 1.0
+ * @since 0.1
+ */
+
+package ru.job4j.rebase;
