a3f33f49feeec3f7f85dfceb9a108a88126462a6
dvamedveda <justmustdie@yandex.ru>
2018-06-02 01:33:05 +0600

    Создал модель User. / 1. Создать модель User[#55656]

---
+++ chapter_004/src/main/java/ru/job4j/map/User.java
@@ -0,0 +1,22 @@
+package ru.job4j.map;
+
+import java.util.Calendar;
+
+/**
+ * Класс описывающий модель User.
+ *
+ * @author - b.savelev (mailto: justmustdie@yandex.ru)
+ * @version - 1.0
+ * @since 0.1
+ */
+public class User {
+    private String name;
+    private int children;
+    private Calendar birthday;
+
+    public User(String name, int children, Calendar birthday) {
+        this.name = name;
+        this.children = children;
+        this.birthday = birthday;
+    }
+}
