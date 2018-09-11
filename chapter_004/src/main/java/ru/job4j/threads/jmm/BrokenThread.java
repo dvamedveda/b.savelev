4f86c9ba57d8103c77227d47776c3b0fee5e9c43
dvamedveda <justmustdie@yandex.ru>
2018-09-11 10:19:06 +0600

    Реализовал приложение, которое иллюстрирует обе проблемы с многопоточностью - data visibility и race condition. / 1. Проиллюстрировать проблемы с многопоточностью.[#70767]

---
+++ chapter_004/src/main/java/ru/job4j/threads/jmm/BrokenThread.java
@@ -0,0 +1,49 @@
+package ru.job4j.threads.jmm;
+
+/**
+ * Приложение иллюстрирующее обе проблемы, возникающие при многопоточности:
+ * первая проблема - data visibility, вторая - race condition.
+ *
+ * @author - b.savelev (mailto: justmustdie@yandex.ru)
+ * @version - 1.0
+ * @since 0.1
+ */
+public class BrokenThread {
+
+    /**
+     * Переменная-счетчик, значение которой будет обновляться двумя потоками.
+     */
+    private int counter = 0;
+
+    /**
+     * Метод для увеличения значения переменной.
+     */
+    private void incrementCounter() {
+        counter++;
+    }
+
+    /**
+     * Инкремент переменной в двух потоках по миллиону раз в каждом.
+     * В конце работы метод выводит итоговое состояние счетчика на печать, которое должно быть равным 2 000 000.
+     */
+    private void doCount() {
+        Thread t1 = new Thread(() -> {for (int i = 0; i < 1000000; i++) {incrementCounter();}});
+        Thread t2 = new Thread(() -> {for (int i = 0; i < 1000000; i++) {incrementCounter();}});
+        t1.start();
+        t2.start();
+
+        while (t1.isAlive() || t2.isAlive()) {
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        System.out.println(counter);
+    }
+
+    public static void main(String[] args) {
+        new BrokenThread().doCount();
+    }
+}
---
+++ chapter_004/src/main/java/ru/job4j/threads/jmm/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * Пакет, содержащий решение задач по JMM.
+ *
+ * @author - b.savelev (mailto: justmustdie@yandex.ru)
+ * @version - 1.0
+ * @since 0.1
+ */
+
+package ru.job4j.threads.jmm;
