38d66f65b61b0c1546fa845566647c5a8b276c1a
dvamedveda <justmustdie@yandex.ru>
2018-11-03 19:58:15 +0600

    Реализовал остановку consumer, если producer закончил свою работу. /  2. Обеспечить остановку потребителя.[#82755]

---
+++ chapter_004/src/main/java/ru/job4j/threads/notify/ParallelSearch.java
@@ -0,0 +1,41 @@
+package ru.job4j.threads.notify;
+
+/**
+ * Реализация блокирующей очереди с прерыванием потребителя при остановке производителя.
+ *
+ * @author - b.savelev (mailto: justmustdie@yandex.ru)
+ * @version - 1.0
+ * @since 0.1
+ */
+public class ParallelSearch {
+
+    public static void main(String[] args) {
+        SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<>();
+        final Thread consumer = new Thread(
+                () -> {
+                    while (!Thread.currentThread().isInterrupted()) {
+                        try {
+                            System.out.println(queue.poll());
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                            Thread.currentThread().interrupt();
+                        }
+                    }
+                }
+        );
+        consumer.start();
+        new Thread(
+                () -> {
+                    for (int index = 0; index != 3; index++) {
+                        try {
+                            queue.put(index);
+                            Thread.sleep(500);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    consumer.interrupt();
+                }
+        ).start();
+    }
+}
