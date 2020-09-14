b1512bf7f871a1c3413aa824a2660c81e48452b2
dvamedveda <justmustdie@yandex.ru>
2020-09-14 10:50:38 +0600

    Написал программу, демонстрирующую вызов сборщика мусора. / 1. Демонстрация работы GC [#361288]

---
+++ chapter_007/src/main/java/ru/job4j/gcdemo/Demo.java
@@ -0,0 +1,68 @@
+package ru.job4j.gcdemo;
+
+/**
+ * Класс демонстрирует работу сборщика мусора.
+ */
+public class Demo {
+
+    /* Размер объекта 64 байт
+     * объект User 16 байт + поле String 48 байт
+     */
+    public static class User {
+
+        /* Размер поля String
+         * сам объект String занимает 16 байт
+         * вложенные в String не-статические поля:
+         * массив value для хранения значений byte[] - объект массива 16 байт + длина массива 4 байта + 4 байт заполнения
+         * кодировка coder, хранящаяся в byte - 1 байт
+         * хешкод hashCode, хранящийся в int - 4 байта
+         * флажок hashIsZero, хранящийся в boolean - 1 байт
+         * заполняющие 2 байта до ^8
+         * итого 16 + 16 + 4 + 4 + 1 + 4 + 1 + 2 = 48 байт
+         */
+        private String name;
+
+        public User(String name) {
+            this.name = name;
+        }
+
+        @Override
+        protected void finalize() throws Throwable {
+            super.finalize();
+            System.out.println("User finalized: " + this.name);
+        }
+
+        @Override
+        public String toString() {
+            return "User name: " + this.name;
+        }
+    }
+
+    /**
+     * В методе демонстрируется создание 10000 объектов и вызов сборщика мусора средствами самой JVM.
+     */
+    public static void main(String[] args) throws Throwable {
+        memoryInfo();
+        for (int i = 0; i < 10000; i++) {
+            if (i % 1000 == 0 & i != 0) {
+                memoryInfo();
+            }
+            User newUser = new User("someuser");
+            newUser = null;
+        }
+    }
+
+    /**
+     * Вывод информации о состоянии памяти.
+     */
+    public static void memoryInfo() {
+        long total = Runtime.getRuntime().maxMemory();
+        long free = Runtime.getRuntime().freeMemory();
+        System.out.println("===========================");
+        System.out.println("Total memory: " + total / 1024 + " Kb");
+        System.out.println("Free memory: " + free / 1024 + " Kb");
+        long used = total - free;
+        System.out.println("Used memory: " + used / 1024 + " Kb");
+        System.out.println("===========================");
+    }
+}
---
+++ chapter_007/src/main/java/ru/job4j/gcdemo/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * Пакет, который содержит классы для работы со сборщиком мусора.
+ *
+ * @author - b.savelev (mailto: justmustdie@yandex.ru)
+ * @version - 1.0
+ * @since 0.1
+ */
+
+package ru.job4j.gcdemo;
